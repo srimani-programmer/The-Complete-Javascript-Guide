380bfd33f561189c57b47225470e2a5397a4e821
Sri Manikanta Palakollu <srimani.scholar@gmail.com>
2021-08-21 15:30:03 +0530

    Concept of Element by id.

---
+++ DOM/scripts/fetch-ele-byid.js
@@ -0,0 +1,13 @@
+const fruits = document.getElementById("fruits");
+console.log(fruits.textContent);
+fruits.style.color = "blue";
+
+const vegetables = document.getElementById("vegetables");
+console.log(vegetables.innerHTML);
+console.log(vegetables.innerText);
+console.log(vegetables.textContent);
+vegetables.style.color = "green";
+
+const cars = document.getElementById("cars");
+console.log(cars.textContent);
+cars.style.color = "violet";
