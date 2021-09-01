00963581918418d4057c22edafd3405392ddb4b9
Sri Manikanta Palakollu <srimani.scholar@gmail.com>
2021-09-02 02:34:02 +0530

    Concept of Maps in JS.

---
+++ Advanced/maps.js
@@ -0,0 +1,45 @@
+// Creation of Maps
+const personMap = new Map();
+
+// Adding elements to the map
+personMap.set("name", "Taylor");
+personMap.set("age", 32);
+personMap.set("profession", "software developer");
+personMap.set("hobbies", ["Football", "Cricket"]);
+
+// Fetch values from Map using keys
+console.log("Name: " + personMap.get("name"));
+console.log("Hobbies: " + personMap.get("hobbies"));
+
+// Iterating the map using the classical for loop
+
+// 1. Fetching Keys from Map
+console.log(
+  "************************ ITERATING KEYS FROM LOOP ************************"
+);
+for (const key of personMap.keys()) {
+  console.log(`Key is: ${key}`);
+}
+
+// 2. Fetching Values from Map
+console.log(
+  "************************ ITERATING VALUES FROM LOOP ************************"
+);
+for (const value of personMap.values()) {
+  console.log(`Value is: ${value}`);
+}
+
+// 3. Fetching both keys and values
+console.log(
+  "************************ ITERATING BOTH KEYS AND VALUES FROM LOOP ************************"
+);
+for (const [key, value] of personMap) {
+  console.log(`Key is: ${key} and value is: ${value}`);
+}
+
+// For Each Loop to fetch the content from map
+console.log("************************ FOR EACH LOOP ************************");
+personMap.forEach((value, key) => {
+  console.log(`Key is: ${key} and the value is: ${value}`);
+});
+// console.log(personMap);
